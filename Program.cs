1ae4d4097d97a5eb9edef7b839e2f99d23ee0219
pawan-1999 <guptapawan767@gmail.com>
2021-07-03 02:08:31 +0530

    Position of players start from 0

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UC_1_SnakeAndLadderPlayedAtPosition0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int POSITION = 0;
+        }
+    }
+}
