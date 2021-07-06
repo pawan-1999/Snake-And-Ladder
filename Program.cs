73c26294177e3f7ca735e00e1324af5024f83787
pawan-1999 <guptapawan767@gmail.com>
2021-07-06 22:42:31 +0530

    To get the winning postion and total die count to win the game

--- Program.cs
+++ Program.cs
@@ -4,58 +4,66 @@ using System.Text;
 
 namespace UC_1_SnakeAndLadderPlayedAtPosition0
 {
-    class UC_5_ExactWinningPosition
+    class UC_6_WiningPositionAndDiceCount
     {
         public const int POSITION = 0;
         public const int WINNING_POSITION = 100;
-        public void exactWinPosition()
+        public void diceCountToWinTheGame()
         {
             int position = 0;
+            int diceCount = 0;
             Console.WriteLine("Snake And Ladder Game ");
             while (position != WINNING_POSITION)
             {
+                diceCount++;
                 Random random = new Random();
                 int player = random.Next(0, 7);
-                Console.WriteLine("Outcomes is : " +player);
+                Console.WriteLine("Outcomes is : " + player);
                 Random random1 = new Random();
                 int option = random1.Next(0, 3);
                 switch (position)
                 {
                     case 0:
-                        Console.WriteLine("Current position is: " +position);
-                        Console.WriteLine("You stay in same position: " +position);
-                        
+                        Console.WriteLine("Current position is: " + position);
+                        Console.WriteLine("You stay in same position: " + position);
+
                         break;
                     case 1:
-                        Console.WriteLine("Your current position is:" +position);
+                        Console.WriteLine("Your current position is:" + position);
                         Console.WriteLine("You encountered a ladder");
-                        if ((position+player) > 100)
+                        if ((position + player) > 100)
                         {
-                            Console.WriteLine("Current position is:" +position);
+                            Console.WriteLine("Current position is:" + position);
                         }
                         else
                         {
                             position = position + player;
-                            Console.WriteLine("Player ladder moves ahead:" +position);
+                            Console.WriteLine("Player ladder moves ahead:" + position);
                         }
                         break;
                     case 2:
                         Console.WriteLine("You encountered a Snake");
-                        Console.WriteLine("Current position is:" +position);
+                        Console.WriteLine("Current position is:" + position);
                         if (position < 0)
                         {
-                            Console.WriteLine("Player snake moves bheind:" +POSITION);
+                            Console.WriteLine("Player snake moves bheind:" + POSITION);
                             position = POSITION;
                         }
                         else
                         {
                             position = position - player;
-                            Console.WriteLine("Player moves behind :" +position);
+                            Console.WriteLine("Player moves behind :" + position);
                         }
                         break;
                 }
+                Console.WriteLine("Dice count : " +diceCount);
             }
         }
-     }
+    }
 }
 
+	static void Main(string[] args)
+        	{
+           	    UC_6_WiningPositionAndDiceCount obj4 = new UC_6_WiningPositionAndDiceCount();
+           	    obj4.diceCountToWinTheGame();
+		} 
