d7aec5bc921931e36bd1afff9920d91e9087d2c8
pawan-1999 <guptapawan767@gmail.com>
2021-07-06 22:33:51 +0530

    To get exact winning position for a player

--- Program.cs
+++ Program.cs
@@ -4,56 +4,58 @@ using System.Text;
 
 namespace UC_1_SnakeAndLadderPlayedAtPosition0
 {
-    class UC_4_WinningPosition
+    class UC_5_ExactWinningPosition
     {
         public const int POSITION = 0;
-        public void winPosition()
+        public const int WINNING_POSITION = 100;
+        public void exactWinPosition()
         {
-            int position = 0, ladderCount = 0, snakeCount = 0;
-            Console.WriteLine("Snake And Ladeer Game ");
-            Random random = new Random();
-            int player = random.Next(0, 7);
-            Console.WriteLine("Outcome is : " + player);
-            Random random1 = new Random();
-            int option = random1.Next(0, 3);
-            //Winning Condition 
-            while (position!=100)
+            int position = 0;
+            Console.WriteLine("Snake And Ladder Game ");
+            while (position != WINNING_POSITION)
             {
-                switch (option)
+                Random random = new Random();
+                int player = random.Next(0, 7);
+                Console.WriteLine("Outcomes is : " +player);
+                Random random1 = new Random();
+                int option = random1.Next(0, 3);
+                switch (position)
                 {
                     case 0:
-                        Console.WriteLine("your current position : " +position);
-                        Console.WriteLine("you stay in same position : " +position);
+                        Console.WriteLine("Current position is: " +position);
+                        Console.WriteLine("You stay in same position: " +position);
+                        
                         break;
                     case 1:
-                        ladderCount++;
-                        Console.WriteLine("current poition is : " +position);
-                        position = position + player;
-                        Console.WriteLine("Player ladder moves ahead : " +position );
-                        break;
-                    case 2:
-                        snakeCount++;
-                        Console.WriteLine("Current position is : " +position);
-                        position = position - player;
-                        if (position < 0)
+                        Console.WriteLine("Your current position is:" +position);
+                        Console.WriteLine("You encountered a ladder");
+                        if ((position+player) > 100)
                         {
-                            Console.WriteLine("Player snake move behind : " + POSITION);
+                            Console.WriteLine("Current position is:" +position);
                         }
                         else
                         {
+                            position = position + player;
+                            Console.WriteLine("Player ladder moves ahead:" +position);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("You encountered a Snake");
+                        Console.WriteLine("Current position is:" +position);
+                        if (position < 0)
+                        {
+                            Console.WriteLine("Player snake moves bheind:" +POSITION);
+                            position = POSITION;
+                        }
+                        else
+                        {
+                            position = position - player;
                             Console.WriteLine("Player moves behind :" +position);
                         }
                         break;
                 }
-
             }
         }
-    }
+     }
 }
 
-  static void Main(string[] args)
-        {
-            const int POSITION = 0;
-            UC_4_WinningPosition obj2 = new UC_4_WinningPosition();
-            obj2.winPosition();
-        }
