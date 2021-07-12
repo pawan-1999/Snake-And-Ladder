cfc3e1f4a2060a981fda06754d01cac615020c5f
Pawan gupta <75203671+pawan-1999@users.noreply.github.com>
2021-07-12 07:55:36 +0530

    Added : Check For Option

--- Program.cs
+++ Program.cs
@@ -12,23 +12,23 @@ namespace UC_1_SnakeAndLadderPlayedAtPosition0
             int position = 0, ladderCount = 0, snakeCount = 0;
             Console.WriteLine("Snake And Ladeer Game ");
             Random random = new Random();
-            int player = random.Next(0, 7);
+            int player = random.Next(0, 6) + 1;
             Console.WriteLine("Outcome is : " + player);
             Random random1 = new Random();
-            int option = random1.Next(0, 3);
+            int option = random1.Next(0, 3) + 1;
             switch (option)
             {
-                case 0:
+                case 1:
                     Console.WriteLine("Current position is : " + position);
                     Console.WriteLine("You stay in same position : " + position);
                     break;
-                case 1:
+                case 2:
                     ladderCount++;
                     Console.WriteLine("Current Position is : " + position);
                     position = position + player;
                     Console.WriteLine("Player Ladder moves ahed : " + position);
                     break;
-                case 2:
+                case 3:
                     snakeCount++;
                     Console.WriteLine("Current Positin is : " + position);
                     position = position - player;
